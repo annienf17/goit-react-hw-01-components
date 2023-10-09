05858ff2eb1a58099be1a73c14030b0c478ff3df
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-10-09 16:13:06 +0100

    profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,49 @@
+import PropTypes from "prop-types";
+import css from '../css/Profile.module.css';
+
+export function Profile ({ user }) {
+    return (
+        <div className={css.profile}>
+        <div className={css.description}>
+          <img 
+            src={user.avatar}
+            alt={user.username}
+            className={css.avatar}
+          />
+          <p className={css.name}>{user.username}</p>
+          <p className={css.tag}>@{user.tag}</p>
+          <p className={css.location}>{user.location}</p>
+        </div>
+      
+        <ul className={css.stats}>
+          <li>
+            <span className={css.label}>Followers</span>
+            <span className={css.quantity}>{user.stats.followers}</span>
+          </li>
+          <li>
+            <span className={css.label}>Views</span>
+            <span className={css.quantity}>{user.stats.views}</span>
+          </li>
+          <li>
+            <span className={css.label}>Likes</span>
+            <span className={css.quantity}>{user.stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    )   
+};
+   
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string,
+    tag: PropTypes.string,
+    location: PropTypes.string,
+    avatar: PropTypes.string,
+    stats: PropTypes.shape({
+      followers: PropTypes.number,
+      views: PropTypes.number,
+      likes: PropTypes.number,
+    }),
+  }),
+};
