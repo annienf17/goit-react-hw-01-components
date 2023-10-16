c9ce811a002d12356fceb49bb118318cd8604ec0
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-10-16 20:17:27 +0100

    random color

--- src/components/StatItems.jsx
+++ src/components/StatItems.jsx
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types';
-import random from '../js/random';
 import css from '../css/Statistics.module.css';
+
+function getRandomHexColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
 
 export function StatItems({ label, percentage }) {
   return (
-    <li className={css.item} style={{ backgroundColor: random() }}>
+    <li className={css.item} style={{ backgroundColor: getRandomHexColor() }}>
       <span className={css.label}>{label}</span>
       <span className={css.percentage}>{percentage}%</span>
     </li>
@@ -15,3 +21,5 @@ StatItems.propTypes = {
   label: PropTypes.string,
   percentage: PropTypes.number,
 };
+
+ 
